065604ef4d60987945b794a41faf71b41387a09c
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-12-09 18:39:17 -0300

    Implement basic board component

---
+++ src/lib/components/Board.svelte
@@ -0,0 +1,28 @@
+<div class='container'>
+    {#each [...Array(8).keys()] as i}
+        {#each [...Array(8).keys()] as j}
+            <div class='square white' class:black={((i + j) % 2 != 0)}/>
+        {/each}
+        <br>
+    {/each}
+</div>
+
+<style>
+    .container {
+        display: inline-block;
+        background-color: red;
+        line-height: 0;
+        box-shadow: 0px 0px 20px 5px black;
+    }
+    .square {
+        display: inline-block;
+        width: 100px;
+        height: 100px;
+    }
+    .white {
+        background-color: white;
+    }
+    .black {
+        background-color: black;
+    }
+</style>
