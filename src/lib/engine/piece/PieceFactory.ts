0052b6154ca5de29e5b5107a02b3c24e2aa08cc3
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-12-09 17:08:01 -0300

    Implement PieceFactory

---
+++ src/lib/engine/piece/PieceFactory.ts
@@ -0,0 +1,32 @@
+import Pawn from '$lib/engine/piece/Pawn'
+import King from '$lib/engine/piece/King'
+import Queen from '$lib/engine/piece/Queen'
+import Bishop from '$lib/engine/piece/Bishop'
+import Knight from '$lib/engine/piece/Knight'
+import Rook from '$lib/engine/piece/Rook'
+
+const pieces = {
+    'p' : Pawn,
+    'k' : King,
+    'q' : Queen,
+    'b' : Bishop,
+    'n' : Knight,
+    'r' : Rook,
+}
+
+const colors = {
+    'w' : 'white',
+    'b' : 'black'
+}
+
+export default class PieceFactory {
+    static create(pieceString) {
+        const colorChar = pieceString[0]
+        const pieceChar = pieceString[1]
+        if (pieceChar in pieces && colorChar in colors) {
+            const piece = pieces[pieceChar]
+            const color = colors[colorChar]
+            return new piece(color)
+        }
+    }
+}
