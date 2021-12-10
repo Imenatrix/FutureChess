da19c80562a9300b6523b512f7a7f80aa01d3f50
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-12-10 16:04:25 -0300

    Make rook not go over other pieces

--- src/lib/engine/piece/Rook.ts
+++ src/lib/engine/piece/Rook.ts
@@ -5,9 +5,20 @@ export default class Rook extends Piece {
     char = 'r'
 
     move(i : number, j : number) {
-        const mi = Math.abs(this.i - i)
-        const mj = Math.abs(this.j - j)
+        const di = i - this.i
+        const dj = j - this.j
+        const si = Math.sign(di)
+        const sj = Math.sign(dj)
+        const mi = Math.abs(di)
+        const mj = Math.abs(dj)
         if (Math.min(mi, mj) == 0) {
+            for (let x = Math.abs(si); x <= mi; x++) {
+                for (let y = Math.abs(sj); y <= mj; y++) {
+                    const sx = x * si
+                    const sy = y * sj
+                    if (this.board[this.i + sx][this.j + sy]) return
+                }
+            }
             super.move(i, j)
         }
     }
