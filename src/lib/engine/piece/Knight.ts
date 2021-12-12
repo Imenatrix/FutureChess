d994b97314e1dc22f5eed791b72741608f117089
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-12-12 16:38:20 -0300

    Implement Pawn taking movement

--- src/lib/engine/piece/Knight.ts
+++ src/lib/engine/piece/Knight.ts
@@ -7,6 +7,8 @@ export default class Knight extends Piece {
     move(i: number, j: number) {
         const mi = Math.abs(this.i - i)
         const mj = Math.abs(this.j - j)
+		
+		if (this.board[i][j]?.color == this.color) return
         if (mi == 1 && mj == 2 || mi == 2 && mj == 1) {
             super.move(i, j)
         }
