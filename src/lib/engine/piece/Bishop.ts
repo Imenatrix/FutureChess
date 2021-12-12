e5e304e4ce566909a3a457a19fce2c8cd202cf67
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-12-12 05:39:16 -0300

    Make Bishop not go throught other pieces

--- src/lib/engine/piece/Bishop.ts
+++ src/lib/engine/piece/Bishop.ts
@@ -5,9 +5,18 @@ export default class Bishop extends Piece{
     char = 'b'
 
     move(i : number, j : number) {
-        const mi = Math.abs(this.i - i)
-        const mj = Math.abs(this.j - j)
+        const di = i - this.i
+        const dj = j - this.j
+        const si = Math.sign(di)
+        const sj = Math.sign(dj)
+        const mi = Math.abs(di)
+        const mj = Math.abs(dj)
         if (mj == mi) {
+			for (let x = Math.abs(si); x <= mi; x++) {
+				const sx = x * si
+				const sy = x * sj
+				if (this.board[this.i + sx][this.j + sy]) return
+            }
             super.move(i, j)
         }
     }
