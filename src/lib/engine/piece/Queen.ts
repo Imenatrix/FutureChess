4cb29aa033e8585ee074b887ed313333f30e3001
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-12-12 05:40:05 -0300

    Make Queen not go throught other pieces

--- src/lib/engine/piece/Queen.ts
+++ src/lib/engine/piece/Queen.ts
@@ -5,11 +5,32 @@ export default class Queen extends Piece {
     char = 'q'
 
     move(i : number, j : number) {
-        const mi = Math.abs(this.i - i)
-        const mj = Math.abs(this.j - j)
-        if (Math.min(mi, mj) == 0 || mi == mj) {
-            super.move(i, j)
+        const di = i - this.i
+        const dj = j - this.j
+        const si = Math.sign(di)
+        const sj = Math.sign(dj)
+        const mi = Math.abs(di)
+        const mj = Math.abs(dj)
+        if (Math.min(mi, mj) == 0) { 
+			for (let x = Math.abs(si); x <= mi; x++) {
+                for (let y = Math.abs(sj); y <= mj; y++) {
+                    const sx = x * si
+                    const sy = y * sj
+                    if (this.board[this.i + sx][this.j + sy]) return
+                }
+            }
         }
+		else if (mi == mj) {
+			for (let x = Math.abs(si); x <= mi; x++) {
+				const sx = x * si
+				const sy = x * sj
+				if (this.board[this.i + sx][this.j + sy]) return
+            }
+		}
+		else {
+			return
+		}
+		super.move(i, j)
     }
 
 }
