6fe8fb02b2d0a14cb1f3a130a17711c3efc26c52
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-12-12 17:22:19 -0300

    Implement Pawn double move

--- src/lib/engine/piece/Pawn.ts
+++ src/lib/engine/piece/Pawn.ts
@@ -4,6 +4,7 @@ export default class Pawn extends Piece {
 
     char = 'p'
     direction : 1 | -1
+	moved = false
 
     constructor(color : string, board : Array<Array<Piece>>, i : number, j : number) {
         super(color, board, i, j)
@@ -18,16 +19,22 @@ export default class Pawn extends Piece {
     move(i: number, j: number) {
         const di = i - this.i
         const dj = j - this.j
-        if (di == this.direction && dj == 0) {
+		if (!this.moved && di == this.direction * 2 && dj == 0) {
+			if (this.board[i][j]) return
+		}
+		else if (di == this.direction && dj == 0) {
             if (this.board[i][j]) return
-            super.move(i, j)
         }
 		else if(di == this.direction && Math.abs(dj) == 1) {
 			if (this.board[i][j]) {
 				if (this.board[i][j]?.color == this.color) return
-				super.move(i, j)
 			}
 		}
+		else {
+			return
+		}
+		super.move(i, j)
+		this.moved = true
     }
 
 }
