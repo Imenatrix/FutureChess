d076d152411c3db1b0df1ec4a725a1e17e730a5c
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-12-12 21:27:51 -0300

    Use sprites in Piece

--- src/lib/components/Piece.svelte
+++ src/lib/components/Piece.svelte
@@ -12,16 +12,16 @@
 	onMount(() => {
 		const mousemove = (event : MouseEvent) => {
 			if (container.style.position == 'fixed') {
-				container.style.top = event.pageY - 25 + 'px'
-				container.style.left = event.pageX - 25 + 'px'
+				container.style.top = event.pageY - 50 + 'px'
+				container.style.left = event.pageX - 50 + 'px'
 				window.getSelection().removeAllRanges()
 			}
 		}
 		const mouseup = () => {
 			container.style.pointerEvents = 'auto'
 			container.style.position = 'absolute'
-			container.style.top = '25%'
-			container.style.left = '25%'
+			container.style.top = '0'
+			container.style.left = '0'
 			container.style.zIndex = '0'
 		}
 		document.addEventListener('mousemove', mousemove)
@@ -38,27 +38,23 @@
 		container.style.pointerEvents = 'none'
 		container.style.position = 'fixed'
 		container.style.zIndex = '1'
-		container.style.top = event.pageY - 25 + 'px'
-		container.style.left = event.pageX - 25 + 'px'
+		container.style.top = event.pageY - 50 + 'px'
+		container.style.left = event.pageX - 50 + 'px'
 	}
 
 </script>
 
 <div bind:this={container} class='container' on:mousedown={handleMouseDown}>
-	{piece}
+	<img src="/imgs/{piece.char}-{piece.color}.svg" width="100%" alt="">
 </div>
 
 <style>
 	.container {
 		position: absolute;
-		top : 25%;
-		left: 25%;
 		display : flex;
 		justify-content: center;
 		align-items: center;
-		width: 50px;
-		height: 50px;
-		background-color: lightgray;
-		color : black;
+		width: 100px;
+		height: 100px;
 	}
 </style>
