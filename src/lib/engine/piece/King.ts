4e7390655c278b3ff1d8c39e111e4eda2e12c499
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-12-14 01:09:49 -0300

    Implement castle detection on King

--- src/lib/engine/piece/King.ts
+++ src/lib/engine/piece/King.ts
@@ -1,8 +1,10 @@
 import Piece from '$lib/engine/piece/Piece'
+import Rook from './Rook'
 
 export default class King extends Piece{
 
 	char = 'k'
+	moved = false
 
 	canAttack(i : number, j : number) {
 		const di = i - this.i
@@ -40,6 +42,81 @@ export default class King extends Piece{
 		}
 	}
 
+	availableMoves() {
+		return this.board.map((row, i) => row.map((_, j) => this.canMove(i, j) || this.canCastle(i, j)))
+	}
+
+	canCastle(i : number, j : number) {
+		const rook = this.board[i][j]
+		const di = i - this.i
+		const dj = j - this.j
+		const si = Math.sign(di)
+		const sj = Math.sign(dj)
+		const mi = Math.abs(di)
+		const mj = Math.abs(dj)
+
+		const squaresUnderAttack = this.board.map(
+			row => row.filter(piece => piece)
+					  .filter(piece => piece.color != this.color)
+					  .map(piece => piece.canAttack(i, j))
+					  .flat()
+		).flat()
+
+		if (this.moved) return false
+		if (!rook) return false
+		if (!(rook instanceof Rook)) return false
+		if (rook.color != this.color) return false
+		if (rook.moved) return false
+		if (squaresUnderAttack.includes(true)) return false
+		if (mj == 0) {
+			for (let x = Math.abs(si); x < mi; x++) {
+				const sx = x * si
+				const squaresUnderAttack = this.board.map(
+					row => row.filter(piece => piece)
+							  .filter(piece => piece.color != this.color)
+							  .map(piece => piece.canAttack(this.i + sx, this.j))
+							  .flat()
+				).flat()
+
+				if (this.board[this.i + sx][this.j]) return false
+				if (squaresUnderAttack.includes(true)) return false
+			}
+		}
+		else if (mi == 0) {
+			for (let y = Math.abs(sj); y < mj; y++) {
+				const sy = y * sj
+				const squaresUnderAttack = this.board.map(
+					row => row.filter(piece => piece)
+							  .filter(piece => piece.color != this.color)
+							  .map(piece => piece.canAttack(this.i, this.j + sy))
+							  .flat()
+				).flat()
+
+				if (this.board[this.i][this.j + sy]) return false
+				if (squaresUnderAttack.includes(true)) return false
+			}
+		}
+		else if (mi == mj) {
+			for (let x = Math.abs(si); x < mi; x++) {
+				const sx = x * si
+				const sy = x * sj
+				const squaresUnderAttack = this.board.map(
+					row => row.filter(piece => piece)
+							  .filter(piece => piece.color != this.color)
+							  .map(piece => piece.canAttack(this.i + sx, this.j + sy))
+							  .flat()
+				).flat()
+
+				if (this.board[this.i + sx][this.j + sy]) return false
+				if (squaresUnderAttack.includes(true)) return false
+			}
+		}
+		else {
+			return false
+		}
+		return true
+	}
+
 	canMove(i : number, j : number) {
 		const squaresUnderAttack = this.board.map(
 			row => row.filter(piece => piece)
@@ -51,6 +128,14 @@ export default class King extends Piece{
 			return false
 		}
 		return super.canMove(i, j)
+	}
+
+	move(i: number, j: number) {
+		const moved = super.move(i, j)
+		if (!this.moved && moved) {
+			this.moved = true
+		}
+		return moved
 	}
 
 }
