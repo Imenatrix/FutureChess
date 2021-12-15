46cbc912d9bdfc9700f77200dacf985d042852d9
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-12-15 01:44:47 -0300

    Refactor King methods

--- src/lib/engine/piece/King.ts
+++ src/lib/engine/piece/King.ts
@@ -44,24 +44,23 @@ export default class King extends Piece {
 	}
 
 	canMoveCastle(i : number, j : number) {
-		{
-			const di = i - this.i
-			const dj = j - this.j
-			const si = Math.sign(di)
-			const sj = Math.sign(dj)
-			const mi = Math.abs(di)
-			const mj = Math.abs(dj)
-			if (!(Math.max(mi, mj) == 2 && Math.min(mi, mj) == 0)) return false
-			i = si == 1 ? this.board.length - 1 : (si == -1 ? 0 : this.i)
-			j = sj == 1 ? this.board[i].length - 1 : (sj == -1 ? 0 : this.j)
-		}
+		let di = i - this.i
+		let dj = j - this.j
+		let si = Math.sign(di)
+		let sj = Math.sign(dj)
+		let mi = Math.abs(di)
+		let mj = Math.abs(dj)
+		
+		if (!(Math.max(mi, mj) == 2 && Math.min(mi, mj) == 0)) return false
+		i = si == 1 ? this.board.length - 1 : (si == -1 ? 0 : this.i)
+		j = sj == 1 ? this.board[i].length - 1 : (sj == -1 ? 0 : this.j)
 
-		const di = i - this.i
-		const dj = j - this.j
-		const si = Math.sign(di)
-		const sj = Math.sign(dj)
-		const mi = Math.abs(di)
-		const mj = Math.abs(dj)
+		di = i - this.i
+		dj = j - this.j
+		si = Math.sign(di)
+		sj = Math.sign(dj)
+		mi = Math.abs(di)
+		mj = Math.abs(dj)
 
 		if (Math.min(mi, mj) == 0) {
 			for (let x = Math.abs(si); x <= mi; x++) {
@@ -90,6 +89,7 @@ export default class King extends Piece {
 		if (rook.color != this.color) return false
 		if (rook.moved) return false
 		if (this.isPositionUnderAttack(i, j)) return false
+
 		if (Math.min(mi, mj) == 0) {
 			for (let x = Math.abs(si); x <= mi - Math.abs(si); x++) {
 				for (let y = Math.abs(sj); y <= mj - Math.abs(sj); y++) {
@@ -119,49 +119,49 @@ export default class King extends Piece {
 		return super.canMove(i, j)
 	}
 
-	moveCastle(i : number, j : number) {
-		if (this.canMoveCastle(i, j)) {
+	findRook(i : number, j : number) {
+		const di = i - this.i
+		const dj = j - this.j
+		const si = Math.sign(di)
+		const sj = Math.sign(dj)
+		const mi = Math.abs(di)
+		const mj = Math.abs(dj)
 
-			coiso : {
-				{
-					const di = i - this.i
-					const dj = j - this.j
-					const si = Math.sign(di)
-					const sj = Math.sign(dj)
-					const mi = Math.abs(di)
-					const mj = Math.abs(dj)
-					if (!(Math.max(mi, mj) == 2 && Math.min(mi, mj) == 0)) return false
-					i = si == 1 ? this.board.length - 1 : (si == -1 ? 0 : this.i)
-					j = sj == 1 ? this.board[i].length - 1 : (sj == -1 ? 0 : this.j)
-				}
-		
-				const di = i - this.i
-				const dj = j - this.j
-				const si = Math.sign(di)
-				const sj = Math.sign(dj)
-				const mi = Math.abs(di)
-				const mj = Math.abs(dj)
-		
-				if (Math.min(mi, mj) == 0) {
-					for (let x = Math.abs(si); x <= mi; x++) {
-						for (let y = Math.abs(sj); y <= mj; y++) {
-							const sx = x * si
-							const sy = y * sj
-							if (this.board[this.i + sx][this.j + sy] instanceof Rook) {
-								i = this.i + sx
-								j = this.j + sy
-								break coiso
-							}
+		if (Math.min(mi, mj) == 0) {
+			for (let x = Math.abs(si); x <= mi; x++) {
+				for (let y = Math.abs(sj); y <= mj; y++) {
+					const sx = x * si
+					const sy = y * sj
+					if (this.board[this.i + sx][this.j + sy] instanceof Rook) {
+						return {
+							i : this.i + sx,
+							j : this.j + sy
 						}
 					}
 				}
 			}
+		}
+	}
 
+	moveCastle(i : number, j : number) {
+		if (this.canMoveCastle(i, j)) {
+			let di = i - this.i
+			let dj = j - this.j
+			let si = Math.sign(di)
+			let sj = Math.sign(dj)
+			let mi = Math.abs(di)
+			let mj = Math.abs(dj)
+			if (!(Math.max(mi, mj) == 2 && Math.min(mi, mj) == 0)) return false
+			i = si == 1 ? this.board.length - 1 : (si == -1 ? 0 : this.i)
+			j = sj == 1 ? this.board[i].length - 1 : (sj == -1 ? 0 : this.j);
+
+			({i, j} = this.findRook(i, j))
 			const rook = this.board[i][j]
-			const di = i - this.i
-			const dj = j - this.j
-			const si = Math.sign(di)
-			const sj = Math.sign(dj)
+			
+			di = i - this.i
+			dj = j - this.j
+			si = Math.sign(di)
+			sj = Math.sign(dj)
 
 			this.board[this.i][this.j] = undefined
 			this.i = this.i + (si * 2)
